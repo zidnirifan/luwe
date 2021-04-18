f680f3f27bba9b447922efe98fc977461881462b
Zidni Rif'an <Zidni Rif'an>
2021-04-18 21:54:10 +0700

    component hero

---
+++ src/component/hero.js
@@ -0,0 +1,18 @@
+class Hero extends HTMLElement {
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = /*html*/ `
+            <div class="hero">
+                <div className="hero-inner">
+                    <h1 class="hero-title">Temukan Restoran Terbaikmu</h1>
+                    <p class="hero-tagline">Manjakan perut laparmu dengan hidangan lezat dari restoran yang berkualitas</p>        
+                </div>
+            </div>
+        `;
+  }
+}
+
+customElements.define('hero-element', Hero);
