04199ba621de278878a28f2aec87db4dcefe6af8
Zidni Rif'an <Zidni Rif'an>
2021-05-12 21:22:29 +0700

    add favicon

--- src/elements/star.js
+++ src/elements/star.js
@@ -8,17 +8,12 @@ const createStar = (value) => {
   const greyStar = `<img src="${greyStarImg}" class="star"/>`;
   let starComponent = '';
 
-  for (let i = 1; i <= value; i += 1) {
-    starComponent += fullStar;
-  }
+  for (let i = 1; i <= value; i += 1) starComponent += fullStar;
 
-  if (Math.round(value) === Math.ceil(value)) {
-    starComponent += halfStar;
-  }
+  if (Math.round(value) === Math.ceil(value)) starComponent += halfStar;
 
-  for (let j = 1; j <= 5 - Math.round(value); j += 1) {
-    starComponent += greyStar;
-  }
+  for (let j = 1; j <= 5 - Math.round(value); j += 1) starComponent += greyStar;
+
   return starComponent;
 };
 
