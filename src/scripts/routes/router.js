caa960bfc6db4c695b5da001d9943dc69331cfdf
Zidni Rif'an <Zidni Rif'an>
2021-05-22 09:54:56 +0700

    fix bug in router.js

--- src/scripts/routes/router.js
+++ src/scripts/routes/router.js
@@ -3,15 +3,16 @@ const notFound = () => 'Not Found';
 const router = (routeTable, pageNotFound = notFound) => {
   const urls = Object.getOwnPropertyNames(routeTable);
 
-  const loadContent = () => {
+  const loadContent = async () => {
     const main = document.querySelector('main');
-    urls.forEach(async (url) => {
-      if (window.location.hash === url) {
-        main.innerHTML = await routeTable[url]();
-      } else {
-        main.innerHTML = pageNotFound();
-      }
-    });
+    const url = urls.filter((e) => window.location.hash === e)[0];
+
+    if (window.location.hash === url) {
+      main.innerHTML = '<h1>Loading</h1>';
+      main.innerHTML = await routeTable[url]();
+    } else {
+      main.innerHTML = pageNotFound();
+    }
   };
 
   window.addEventListener('hashchange', () => {
