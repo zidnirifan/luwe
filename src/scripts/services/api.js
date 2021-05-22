437d021a8f1ee14f5206e4ad87badc6403c99ab6
Zidni Rif'an <Zidni Rif'an>
2021-05-22 20:18:05 +0700

    cleaning api.js

--- src/scripts/services/api.js
+++ src/scripts/services/api.js
@@ -5,7 +5,6 @@ const apiService = {
     try {
       const response = await fetch(API_ENDPOINT.LIST_RESTAURANTS);
       const responseJson = response.json();
-      console.log(responseJson);
       return responseJson;
     } catch (error) {
       // eslint-disable-next-line no-console
