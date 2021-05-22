3ab6fe785ad781f8a2d1d353f61e131cda2025ad
Zidni Rif'an <Zidni Rif'an>
2021-05-22 20:06:23 +0700

    fix little bug and optimized

--- src/scripts/elements/star.js
+++ src/scripts/elements/star.js
@@ -3,9 +3,9 @@ import halfStarImg from '../../public/images/half-star.svg';
 import greyStarImg from '../../public/images/grey-star.svg';
 
 const createStar = (value) => {
-  const fullStar = `<img src="${fullStarImg}" class="star"/>`;
-  const halfStar = `<img src="${halfStarImg}" class="star"/>`;
-  const greyStar = `<img src="${greyStarImg}" class="star"/>`;
+  const fullStar = `<img src="${fullStarImg}" alt="star" class="star"/>`;
+  const halfStar = `<img src="${halfStarImg}" alt="half-star" class="star"/>`;
+  const greyStar = `<img src="${greyStarImg}" alt="empty-star" class="star"/>`;
   let starComponent = '';
 
   for (let i = 1; i <= value; i += 1) starComponent += fullStar;
--- src/scripts/services/api.js
+++ src/scripts/services/api.js
@@ -5,6 +5,7 @@ const apiService = {
     try {
       const response = await fetch(API_ENDPOINT.LIST_RESTAURANTS);
       const responseJson = response.json();
+      console.log(responseJson);
       return responseJson;
     } catch (error) {
       // eslint-disable-next-line no-console
