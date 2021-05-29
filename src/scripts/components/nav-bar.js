36cdf288dcdceab35493c9ac86fb592c22b90b24
Zidni Rif'an <Zidni Rif'an>
2021-05-29 13:25:59 +0700

    tidy up nav-bar.js and app-shell.js

--- src/scripts/components/nav-bar.js
+++ src/scripts/components/nav-bar.js
@@ -3,51 +3,43 @@ import logo from '../../public/images/logo.svg';
 class Navbar extends HTMLElement {
   connectedCallback() {
     this.render();
+
+    const toggleBtn = this.querySelectorAll(
+      '.sidenav .menu-item, .menu-btn, .sidenav-overlay',
+    );
+    toggleBtn.forEach((btn) => {
+      btn.addEventListener('click', Navbar.toggleSidebar);
+    });
   }
 
-  set openSidebar(event) {
-    this._openSidebar = event;
-    this.render();
-  }
-
-  set closeSidebar(event) {
-    this._closeSidebar = event;
-    this.render();
+  static toggleSidebar() {
+    document.querySelectorAll('.sidenav, .sidenav-overlay').forEach((e) => {
+      e.classList.toggle('active');
+    });
   }
 
   render() {
     this.innerHTML = /* html */ `
-            <nav class="navbar">
-                <a href="/" class="logo"><img src="${logo}" alt="logo luwe"/></a>
-                <button class="menu-btn" aria-label="menu button">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-                <ul class="menu-list">
-                    <li><a class="menu-item" href="#">Home</a></li>
-                    <li><a class="menu-item" href="#/favorite">Favorite</a></li>
-                    <li><a class="menu-item" href="#/about">About Us</a></li>
-                </ul>
-              </nav>
-                <ul class="sidenav">
-                    <li><a class="menu-item" href="#">Home</a></li>
-                    <li><a class="menu-item" href="#/favorite">Favorite</a></li>
-                    <li><a class="menu-item" href="#/about">About Us</a></li>
-                </ul>
-            <div class="sidenav-overlay"></div>
-        `;
-
-    const menuBtn = this.querySelector('.menu-btn');
-    menuBtn.addEventListener('click', this._openSidebar);
-
-    const overlay = this.querySelector('.sidenav-overlay');
-    overlay.addEventListener('click', this._closeSidebar);
-
-    const menuSidenav = this.querySelectorAll('.sidenav .menu-item');
-    menuSidenav.forEach((menuItem) => {
-      menuItem.addEventListener('click', this._closeSidebar);
-    });
+      <nav class="navbar">
+          <a href="/" class="logo"><img src="${logo}" alt="logo luwe"/></a>
+          <button class="menu-btn" aria-label="menu button">
+              <span></span>
+              <span></span>
+              <span></span>
+          </button>
+          <ul class="menu-list">
+              <li><a class="menu-item" href="#">Home</a></li>
+              <li><a class="menu-item" href="#/favorite">Favorite</a></li>
+              <li><a class="menu-item" href="#/about">About Us</a></li>
+          </ul>
+        </nav>
+          <ul class="sidenav">
+              <li><a class="menu-item" href="#">Home</a></li>
+              <li><a class="menu-item" href="#/favorite">Favorite</a></li>
+              <li><a class="menu-item" href="#/about">About Us</a></li>
+          </ul>
+      <div class="sidenav-overlay"></div>
+    `;
   }
 }
 
