5aea87697eaa99a5de1e98d35a6e7228694dc496
Zidni Rif'an <Zidni Rif'an>
2021-06-01 21:25:28 +0700

    add favorite page

--- src/scripts/pages/favorite.js
+++ src/scripts/pages/favorite.js
@@ -1,3 +1,22 @@
-const favorite = () => '<h1>Favorite</h1>';
+import '../components/list-restaurants';
+import favoriteRestaurantIdb from '../services/idb';
+
+const favorite = async () => {
+  const data = await favoriteRestaurantIdb.getAllRestaurants();
+
+  const listRestaurants = document.createElement('list-restaurants');
+  listRestaurants.data = data;
+
+  let sectionTitle = 'Restoran Favorit';
+
+  if (listRestaurants.innerHTML === '') {
+    sectionTitle = 'Belum ada restoran favorit';
+  }
+
+  return /* html */ `
+  <h2 class="section-title">${sectionTitle}</h2>
+    ${listRestaurants.outerHTML}
+  `;
+};
 
 export default favorite;
