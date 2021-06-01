4be612f97e692d83ab3bb20dca6bdd7e0521ac9c
Zidni Rif'an <Zidni Rif'an>
2021-06-01 20:09:23 +0700

    add active menu-item feature

--- src/scripts/app-shell.js
+++ src/scripts/app-shell.js
@@ -2,3 +2,24 @@ import './components/nav-bar';
 
 const navBar = document.createElement('nav-bar');
 document.querySelector('header').appendChild(navBar);
+
+const menuActive = () => {
+  const menuItem = document.querySelectorAll('.menu-item');
+  const menuItemActive = document.querySelectorAll('.menu-item.active');
+
+  menuItemActive.forEach((e) => e.classList.remove('active'));
+
+  menuItem.forEach((e) => {
+    const href = e.getAttribute('href').substring(1);
+
+    if (window.location.hash.substring(1) === href) e.classList.add('active');
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  menuActive();
+});
+
+window.addEventListener('hashchange', () => {
+  menuActive();
+});
