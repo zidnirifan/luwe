70bd1e78c27a677e0c8a251aef98d5dc023a9990
Zidni Rif'an <Zidni Rif'an>
2021-06-02 09:38:36 +0700

    about page

--- src/scripts/pages/about.js
+++ src/scripts/pages/about.js
@@ -1,3 +1,14 @@
-const about = () => '<h1>Hello</h1>';
+// eslint-disable-next-line arrow-body-style
+const about = () => {
+  const height = window.innerHeight - 130;
+
+  return /* html */ `
+    <div class="about-page" style="height: ${height}px; text-align: center;">
+      <a href="https://linkedin.com/in/zidnirifan" target="_blank">
+        <h1>Linkedin</h1>
+      </a>
+    </div>
+    `;
+};
 
 export default about;
