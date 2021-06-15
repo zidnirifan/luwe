e566b6d230d177788f6a27171db4a98823fd2d39
Zidni Rif'an <Zidni Rif'an>
2021-06-15 22:58:17 +0700

    fix favicon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,7 @@ import './utils/sw-register';
 import '../scss/main.scss';
 import './routes/routes';
 import './app-shell';
+import favicon from '../public/images/favicon.png';
+
+// to add favicon
+document.getElementById('favicon').href = favicon;
