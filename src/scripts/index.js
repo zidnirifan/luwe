6561abc0706a9cb559ed712bd45f31b73ace02f0
Zidni Rif'an <Zidni Rif'an>
2021-06-18 13:43:54 +0700

    change urutan import

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import './utils/sw-register';
 import '../scss/main.scss';
-import './routes/routes';
 import './app-shell';
+import './routes/routes';
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import favicon from '../public/images/favicon.png';
