6f21de70a7731ae280f1fa47a1fff44c8fa1bc30
Zidni Rif'an <Zidni Rif'an>
2021-06-18 19:04:18 +0700

    tidy up sharp.js

--- sharp.js
+++ sharp.js
@@ -10,40 +10,20 @@ if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
 }
 
+const resizeImage = ({ image, size, name }) => {
+  sharp(image)
+    .resize(size)
+    .toFile(
+      path.resolve(
+        __dirname,
+        `${destination}/${image.slice(18).split('.').slice(0, -1).join('.')}-${name}.jpg`,
+      ),
+    );
+};
+
 target.forEach((image) => {
-  sharp(image)
-    .resize(1000)
-    .toFile(
-      path.resolve(
-        __dirname,
-        `${destination}/${image.slice(18).split('.').slice(0, -1).join('.')}-xl.jpg`,
-      ),
-    );
-
-  sharp(image)
-    .resize(800)
-    .toFile(
-      path.resolve(
-        __dirname,
-        `${destination}/${image.slice(18).split('.').slice(0, -1).join('.')}-large.jpg`,
-      ),
-    );
-
-  sharp(image)
-    .resize(600)
-    .toFile(
-      path.resolve(
-        __dirname,
-        `${destination}/${image.slice(18).split('.').slice(0, -1).join('.')}-medium.jpg`,
-      ),
-    );
-
-  sharp(image)
-    .resize(400)
-    .toFile(
-      path.resolve(
-        __dirname,
-        `${destination}/${image.slice(18).split('.').slice(0, -1).join('.')}-small.jpg`,
-      ),
-    );
+  resizeImage({ image, size: 1000, name: 'xl' });
+  resizeImage({ image, size: 800, name: 'large' });
+  resizeImage({ image, size: 600, name: 'medium' });
+  resizeImage({ image, size: 400, name: 'small' });
 });
